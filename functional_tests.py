e74660cded5b19c16270f73d1862f834758435c2
zt19994 <zt191610942@163.com>
2017-09-26 21:10:28 +0800

    show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 """Functional Test 功能测试 FT"""
 
 
@@ -22,28 +22,29 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 打开首页
+        # 你打开了首页
         self.browser.get('http://localhost:8000')
 
-        #  注意到在线待办事项应用网页的标题和头部都包含“To-Do”这个词
+        #  你注意到在线待办事项应用网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # 输入一个待办事项
+        # 你要输入了第一个待办事项，你发现在输入框中有'Enter a to-do item'
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # 在文本框中输入了“Buy peacock feathers”
+        # 你在文本框中输入了“Buy peacock feathers”
         inputbox.send_keys('Buy peacock feathers')
 
-
-        # 按回车，页面更新了
+        # 按回车，你发现页面更新了
         # 在待办事项表格中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows),
@@ -52,12 +53,12 @@ class NewVisitorTest(unittest.TestCase):
         #     )
         # )
 
-
         # 页面又显示了第二个文本框
         # 在第二个文本框中输入第二个待办事项“Use peacock feathers to make a fly”
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 再次更新页面，显示两个待办事项
         self.check_for_row_in_list_table('1: Buy peacock feathers')
